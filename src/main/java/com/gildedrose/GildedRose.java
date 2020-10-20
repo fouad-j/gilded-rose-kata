648bb58efec9c89d937b1815eb2679bb40564712
Fouad J <fouad.j@live.fr>
2020-10-20 21:58:16 +0200

    remove unnecessary condition for backstage

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -24,15 +24,11 @@ class GildedRose {
 
                             if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                                 if (item.sellIn < 11) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
+                                    item.quality = item.quality + 1;
                                 }
 
                                 if (item.sellIn < 6) {
-                                    if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
-                                    }
+                                    item.quality = item.quality + 1;
                                 }
                             }
                         }
