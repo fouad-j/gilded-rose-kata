f878595b956e126c5561c18f07ed7ddad833c7f1
Fouad J <fouad.j@live.fr>
2020-10-21 18:34:35 +0200

    add check to cover scenario when aged brie didnt get older yet

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -59,8 +59,9 @@ class GildedRoseTest {
         assertThat(item.quality).isLessThanOrEqualTo(50);
     }
 
-    @ParameterizedTest(name = "Aged Brie actually increases in Quality the older it gets")
+    @ParameterizedTest(name = "Aged Brie increases in Quality by 2 when it gets older")
     @CsvSource({
+            "Aged Brie, -50, -51, 50, 50",
             "Aged Brie, -50, -51, 49, 50",
             "Aged Brie, -2, -3, 44, 46",
             "Aged Brie, -1, -2, 7, 9",
@@ -82,28 +83,27 @@ class GildedRoseTest {
     }
 
 
-//    @ParameterizedTest(name = "Aged Brie actually increases in Quality the older it gets")
-//    @CsvSource({
-//            "Aged Brie, -2, 10, 12",
-//            "Aged Brie, -1, 10, 12",
-//            "Aged Brie, 0, 10",
-//            "Aged Brie, 1, 10",
-//            "Aged Brie, 2, 10",
-//            "Aged Brie, 7, 10",
-//            "Aged Brie, 8, 10",
-//            "Aged Brie, 10, 10",
-//            "Aged Brie, 15, 10",
-//            "Aged Brie, 49, 10",
-//    })
-//    void should_increase_quality_by_2_when_quality_of_AgedBrie_gets_older(String itemName, int itemSellIn, int itemQuality) {
-//        // GIVEN
-//        Item item = new Item(itemName, itemSellIn, itemQuality);
-//
-//        // WHEN
-//        gildedRose.updateItem(item);
-//        System.out.println(item);
-//
-//        // THEN
-//        assertThat(item).isNotNull();
-//    }
+    @ParameterizedTest(name = "Aged Brie increases in Quality by 1 when it didnt get older yet")
+    @CsvSource({
+            "Aged Brie, 1, 0, 49, 50",
+            "Aged Brie, 2, 1, 20, 21",
+            "Aged Brie, 7, 6, 35, 36",
+            "Aged Brie, 10, 9, 11, 12",
+            "Aged Brie, 15, 14, 50, 50",
+            "Aged Brie, 50, 49, 10, 11",
+    })
+    void should_increase_quality_by_1_when_quality_of_AgedBrie_gets_older(String itemName, int itemSellIn, int expectedSellIn, int itemQuality, int expectedQuality) {
+        // GIVEN
+        Item item = new Item(itemName, itemSellIn, itemQuality);
+
+        // WHEN
+        gildedRose.updateItem(item);
+        System.out.println(item);
+
+        // THEN
+        SoftAssertions soft = new SoftAssertions();
+        soft.assertThat(item.quality).isEqualTo(expectedQuality);
+        soft.assertThat(item.sellIn).isEqualTo(expectedSellIn);
+        soft.assertAll();
+    }
 }
