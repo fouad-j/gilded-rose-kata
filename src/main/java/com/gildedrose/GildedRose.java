8e4d4f254fb31c760e91006cdca58b21335c0bf8
Fouad J <fouad.j@live.fr>
2020-10-21 20:27:24 +0200

    add tests for backstage passes

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -19,7 +19,7 @@ class GildedRose {
     //“Aged Brie” actually increases in Quality the older it gets                           TESTED
     //The Quality of an item is never more than 50                                          TESTED(add test to avoid init item by value more than 50)
     //“Sulfuras”, being a legendary item, never has to be sold or decreases in Quality      TESTED
-    //“Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
+    //“Backstage passes”, like aged brie, increases in Quality as it’s SellIn value approaches; Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert  TESTED
 
     //Aged Brie
     //Backstage passes to a TAFKAL80ETC concert
@@ -62,13 +62,13 @@ class GildedRose {
                 }
             }
         } else {
-            if (item.sellIn < 0) {
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 0) {
                     item.quality = 0;
-                } else {
-                    if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
+                }
+            } else {
+                if (item.sellIn < 0 && item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
                 }
             }
         }
